39a5e97b00905fed604fc6b5b7a8da80e51d6e56
Dinesh <dkumar.270985@gmail.com>
2020-05-03 16:23:19 +0530

    Adding an Item and Deleting an Item from Items

--- app.js
+++ app.js
@@ -1,34 +1,78 @@
 const express=require("express");
 const bodyparser=require("body-parser")
+const mongoose=require("mongoose")
 const date=require(__dirname+"/date.js")
  
 const app=express();
 let ejs = require('ejs')
-let items=['Buy Food','Cook Food','Bake Food']
 let workItems=[];
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static("public"))
+mongoose.connect("mongodb://localhost:27017/todoListDB",{ useNewUrlParser: true })
+
+const Itemschema={
+    name : String
+}
+
+const Item=mongoose.model("Item",Itemschema);
+const Item1=new Item({
+    name:"Eating"
+})
+const Item2=new Item({
+    name:"Sleeping"
+})
+const Item3=new Item({
+    name:"Drinking"
+})
+
+const documents=[Item1,Item2,Item3];
+
+
 
 app.set("view engine",'ejs');
 app.get("/",function(req,res){
-  
-    let day=date.getDay();
 
-    res.render("list",{listTitle:day,newListItem:items})
+
+  
+   Item.find({},function(err,foundItems){
+       if(foundItems.length===0){
+        Item.insertMany(documents,function(err){
+            if(err){
+                console.log("Error in inserting the data");
+                }
+            else{
+                console.log("Inserted successfully");
+                
+            }
+        });
+        res.redirect("/")
+       }
+    res.render("list",{listTitle:"Today",newListItem:foundItems})
+      
+   })
+
+   
 })
 
 app.post("/",function(request,response){
-    let item=request.body.newItem;
-    if(request.body.list==='Work'){
-        workItems.push(item)
-        response.redirect('/work')
-    }else{
-        items.push(item);
-        response.redirect('/')
-    }
+    const newitem=request.body.newItem;
+    const item=new Item({
+        name: newitem
+    })
+    item.save();
     
     //items.push(item)
-   // response.redirect("/");
+   response.redirect("/");// this will direct to app.get("/")
+})
+
+app.post("/delete",function(req,res){
+    const checkedItemId=req.body.checkboxvalue;
+    Item.findByIdAndRemove(checkedItemId,function(err){
+        console.log("The Checked Item has been successfully deleted");
+        
+    })
+    res.redirect("/")
+    
 })
 
 
